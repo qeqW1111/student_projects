bad326aad2b44b3c48f3f861d8598eb1b658863b
qeqW1111 <jan.szlapinski@gmail.com>
2024-03-15 11:14:25 +0000

    wyslanie pracy

--- python/cwpython1/main.py
+++ python/cwpython1/main.py
@@ -1,1 +1,15 @@
-print ("witaj swiecie")
+x =int (input("podaj liczbe: "))
+
+
+
+if x>0:
+	print ("jest wieksze niz 0")
+
+if x<0:
+	print ("jest mniejsze od 0")
+
+if x==0:
+	print ("jest rowne 0")
+
+
+print ("twoja liczba to: " + str (x))
